a135d5ebe403273409258fd5dae3639f6dfd1bdb
Emmanuel John <emmanuelhashy@gmail.com>
2022-09-26 21:10:53 +0100

    added key props to resolve build issue

--- components/Home/Winners/index.tsx
+++ components/Home/Winners/index.tsx
@@ -4,10 +4,10 @@ import WinnerCard from '../WinnerCard'
 const Winners = () => {
     return (
         <div className='py-10 space-y-6'>
-            <h3 className='text-white text-xl'><span className='font-bold'>This week's stars</span> on Afriguild</h3>
+            <h3 className='text-white text-xl'><span className='font-bold'>This week&apos;s stars</span> on Afriguild</h3>
             <div className='sm:grid gap-4 grid-cols-12'>
                 {[1, 2, 3, 4].map(idx => (
-                    <div className='md:col-span-3 sm:col-span-6'>
+                    <div key={idx} className='md:col-span-3 sm:col-span-6'>
                         <WinnerCard />
                     </div>
                 ))
